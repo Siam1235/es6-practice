63bffedf887c8d4e610d53fe2be614e694430545
Taufiq Azad Siam <towfiqazadsiam@gmail.com>
2021-02-03 07:58:48 +0600

    onek kichu shikhlam

---
+++ constlet.js
@@ -0,0 +1,9 @@
+let students = [
+    {id: 47, name: "Salman Omar"},
+    {id: 98, name: "Sadhin Ahnaf"},
+    {id: 102, name: "Tamim Zabir"},
+    {id: 37, name: "Jubayed Hasan Jhumon"},
+];
+
+const studentFinder = students.map(x => x.name);
+console.log(studentFinder);
